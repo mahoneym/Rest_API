cc7b7c031a962aa3850c7486ed515db3f2c0d09d
maggiemahoney <mahoneym6@xavier.edu>
2019-11-24 14:26:27 -0500

    comments for the getUser method

--- HelloWorld/Hello.py
+++ HelloWorld/Hello.py
@@ -21,11 +21,11 @@ def addUsers():                                             # the method called 
     users.append(user)                                      # put the user in the list
     return jsonify({"user": user}), 201                     # return the user in JSON and a success code
 
-@app.route('/api/v1.0/getUser/<string:name>', methods=['GET'])
-def getUser(name):
-    for user in users:
-        if user["name"] == name:
-            return jsonify({'users': user})
+@app.route('/api/v1.0/getUser/<string:name>', methods=['GET'])      # going to http://127.0.0.1:5000/api/v1.0/getUser/<name>
+def getUser(name):                                          # the method called when going to the URL above
+    for user in users:                                      # go through each user in the list
+        if user["name"] == name:                            # check if the current user is the one I am looking for
+            return jsonify({'users': user})                 # return the user if I have found it
 
 if __name__ == '__main__':                                  # the program starts here
     app.run(debug = True)                                   # run the app

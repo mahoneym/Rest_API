803b3546fb2b2d04f77c30269b909a9d22582d26
maggiemahoney <mahoneym6@xavier.edu>
2019-11-23 22:18:17 -0500

    adding the python file

---
+++ HelloWorld/Hello.py
@@ -0,0 +1,11 @@
+#!flask/bin/python
+from flask import Flask
+
+app = Flask(_name_)
+
+@app.route('/')
+def index():
+    return 'Hello, world!'
+
+if _name_ = '__main__':
+    app.run(debug = True)

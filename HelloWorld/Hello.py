d006170d311bbfe86fa1d2644057edfd4f35411e
maggiemahoney <mahoneym6@xavier.edu>
2019-11-24 13:10:07 -0500

    commenting

--- HelloWorld/Hello.py
+++ HelloWorld/Hello.py
@@ -3,23 +3,23 @@ from flask import Flask, request, jsonify, abort
 
 app = Flask(__name__)
 
-users = []
+users = []                        # the place in memory to store the users instead of a DB
 
-@app.route('/')
+@app.route('/')                   # where to go when going to 127.0.0.1:5000/
 def index():
-    return 'Hello, world!'
+    return 'Hello, world!'        # prints Hello, world! to the screen
 
-@app.route('/api/v1.0/addUsers', methods=['POST'])
-def addUsers():
+@app.route('/api/v1.0/addUsers', methods=['POST'])  # where to go when going to 127.0.0.1:5000/api/v1.0/addUsers
+def addUsers():                                     # the method called when going to the address above
     if not request.json: #or not 'name' in request.json:
         abort(400)
-    user = {
-        "name":request.json["name"],
-        "age": request.json["age"],
-        "occupation": request.json["occupation"]
+    user = {                                        # create a user
+        "name":request.json["name"],                # get the name from the request
+        "age": request.json["age"],                 # get the age from the request
+        "occupation": request.json["occupation"]    # get the occupation from the request
     }
-    users.append(user)
-    return jsonify({"user": user}), 201
+    users.append(user)                              # put the user in the list
+    return jsonify({"user": user}), 201             # return the user in JSON and a success code
 
-if __name__ == '__main__':
-    app.run(debug = True)
+if __name__ == '__main__':                          # the program starts here
+    app.run(debug = True)                           # run the app

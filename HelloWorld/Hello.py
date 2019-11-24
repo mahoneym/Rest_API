0f2c5e6107edfbddcd803b7315b901a7204e5378
maggiemahoney <mahoneym6@xavier.edu>
2019-11-23 23:36:56 -0500

    added POST method to add users

--- HelloWorld/Hello.py
+++ HelloWorld/Hello.py
@@ -1,11 +1,25 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+users = []
 
 @app.route('/')
 def index():
     return 'Hello, world!'
+
+@app.route('/api/v1.0/addUsers/', methods=['POST'])
+def addUsers():
+    if not request.json or not 'name' in request.json:
+        abort(400)
+    user = {
+        'name':request.json['name'],
+        'age': request.json['age'].
+        'occupation': request.json['occupation']
+    }
+    users.append(user)
+    return jsonify({'user': user}), 201
 
 if __name__ == '__main__':
     app.run(debug = True)

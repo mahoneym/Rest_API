2cc056856f44231e5069ff29e05adf5bfa0fc7a9
maggiemahoney <mahoneym6@xavier.edu>
2019-11-24 13:11:24 -0500

    line up the comments and put error cause back in

--- HelloWorld/Hello.py
+++ HelloWorld/Hello.py
@@ -9,17 +9,17 @@ users = []                        # the place in memory to store the users inste
 def index():
     return 'Hello, world!'        # prints Hello, world! to the screen
 
-@app.route('/api/v1.0/addUsers', methods=['POST'])  # where to go when going to 127.0.0.1:5000/api/v1.0/addUsers
-def addUsers():                                     # the method called when going to the address above
-    if not request.json: #or not 'name' in request.json:
-        abort(400)
-    user = {                                        # create a user
-        "name":request.json["name"],                # get the name from the request
-        "age": request.json["age"],                 # get the age from the request
-        "occupation": request.json["occupation"]    # get the occupation from the request
+@app.route('/api/v1.0/addUsers', methods=['POST'])          # where to go when going to 127.0.0.1:5000/api/v1.0/addUsers
+def addUsers():                                             # the method called when going to the address above
+    if not request.json: or not 'name' in request.json:
+        abort(400)                                          # error has occurred: ABORT
+    user = {                                                # create a user
+        "name":request.json["name"],                        # get the name from the request
+        "age": request.json["age"],                         # get the age from the request
+        "occupation": request.json["occupation"]            # get the occupation from the request
     }
-    users.append(user)                              # put the user in the list
-    return jsonify({"user": user}), 201             # return the user in JSON and a success code
+    users.append(user)                                      # put the user in the list
+    return jsonify({"user": user}), 201                     # return the user in JSON and a success code
 
-if __name__ == '__main__':                          # the program starts here
-    app.run(debug = True)                           # run the app
+if __name__ == '__main__':                                  # the program starts here
+    app.run(debug = True)                                   # run the app
